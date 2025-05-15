8ddd57384ee73078dc5c5d3e5b124df3752b232b
joynul24 <joynula919@gmail.com>
2025-05-15 18:38:28 +0600

    added new user gifted coin purchase some API

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+// const corsOptions = {
+//   origin: ['http://localhost:5173/'],
+//   Credential: true,
+//   optionSuccessStatus: 200
+// }
 
 app.use(cors());
 app.use(express.json());
@@ -26,6 +32,96 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db('earnZoneDB').collection('users')
+    const reviewsCollection = client.db('earnZoneDB').collection('reviews')
+
+
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    // get the user coin api
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email.toLowerCase();
+      const user = await usersCollection.findOne({ email });
+      if (!user) {
+        return res.send({ email, coin: 0 });
+      }
+
+      res.send(user);
+
+    })
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const existingUser = await usersCollection.findOne({ email: user.email });
+
+      if (existingUser) {
+        return res.status(200).send({ message: "User already exists" });
+      }
+
+      const result = await usersCollection.insertOne({
+        email: user.email,
+        name: user.name,
+        image: user.image,
+        coin: user.coin || 0,
+        role: user.role || "user",
+      });
+
+      res.status(201).send(result);
+    });
+
+
+    // Coin Update API
+    // app.post('/update-coin', async (req, res) => {
+    //   const { userId, amount } = req.body;
+
+    //   if (!userId || typeof amount !== 'number') {
+    //     return res.status(400).json({ success: false, message: 'Invalid data' });
+    //   }
+
+    //   try {
+    //     const user = await User.findById(userId);
+    //     if (!user) {
+    //       return res.status(404).json({ success: false, message: 'User not found' });
+    //     }
+
+    //     user.coins += amount;
+
+    //     // Coin কখনো negative হলে না রাখতে চাইলে:
+    //     if (user.coins < 0) user.coins = 0;
+
+    //     await user.save();
+    //     res.json({ success: true, coins: user.coins });
+    //   } catch (err) {
+    //     res.status(500).json({ success: false, message: 'Server error' });
+    //   }
+    // });
+
+
+
+    app.post('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = req.body;
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        return res.send(isExist)
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -41,9 +137,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello I am Earn Zone server')
+  res.send('Hello I am Earn Zone server')
 });
 
 app.listen(port, () => {
-    console.log(`Server is running port: ${port}`);
+  console.log(`Server is running port: ${port}`);
 })
